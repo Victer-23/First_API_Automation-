a295abb3767c238440c086a28f0915797bad4fa5
Victer-23 <poojaspawar6555@gmail.com>
2026-05-26 22:28:59 +0530

    Add files via upload

---
+++ payload.py
@@ -0,0 +1,9 @@
+def addBookPayload(isbn):
+    Body = {
+
+        "name":"new AI tools",
+        "isbn":isbn,
+        "aisle":"227",
+        "author":"John foe"
+    }
+    return Body
---
+++ payload_deleteBook.py
@@ -0,0 +1,8 @@
+def delete_book(book_id):
+    Body1 = {
+
+        "ID" : book_id
+
+    }
+
+    return Body1
